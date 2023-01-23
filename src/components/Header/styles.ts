bc47636ea7fdcd8570c3d307028e9ebdc7fb6775
MatheusTA <matrajano12@gmail.com>
2023-01-22 21:44:00 -0300

    feat: create header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+    gap: 0.5rem;
+
+    a {
+      width: 3rem;
+      height: 3rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: ${({ theme }) => theme.colors['primary-100']};
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+
+      &:hover {
+        border-bottom: 3px solid ${({ theme }) => theme.colors['secundary-500']};
+      }
+
+      &.active {
+        color: ${({ theme }) => theme.colors['secundary-500']};
+      }
+    }
+  }
+`
