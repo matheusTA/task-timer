5c09175e670aaaea7806a7e58f4379dc0cb024fd
MatheusTA <matrajano12@gmail.com>
2023-01-25 02:36:06 -0300

    feat: create history laytou

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,5 +1,65 @@
-// import { Container } from './styles';
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
-  return <h1>HISTORY</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Estudar React</td>
+              <td>25 minutos</td>
+              <td>10:00</td>
+              <td>
+                <Status statusColor="error">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>25 minutos</td>
+              <td>10:00</td>
+              <td>
+                <Status statusColor="success">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>25 minutos</td>
+              <td>10:00</td>
+              <td>
+                <Status statusColor="pending">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>25 minutos</td>
+              <td>10:00</td>
+              <td>
+                <Status statusColor="success">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>25 minutos</td>
+              <td>10:00</td>
+              <td>
+                <Status statusColor="pending">Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
